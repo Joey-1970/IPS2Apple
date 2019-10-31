d9f3602919b7a6273d9c922fb2a152e7e2168ebc
Joey-1970 <paeper@horburg.de>
2019-10-31 08:33:09 +0100

    Update module.php

--- IPS2AppleDevice/module.php
+++ IPS2AppleDevice/module.php
@@ -92,7 +92,7 @@
 		If ($this->HasActiveParent() == true) {
 			$this->SetStatus(102);	
 			$this->SendDataToParent(json_encode(Array("DataID"=> "{485663CC-3911-FAC7-9FCC-6E4D06438527}", 
-					 "Function" => "get_Data")));
+					 "Function" => "getData")));
 
 		}
 		else {
