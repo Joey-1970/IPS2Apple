431bfb8935c6757360e1e85fdaada4ac5127f66b
Joey-1970 <paeper@horburg.de>
2019-10-31 19:54:53 +0100

    Update module.php

--- IPS2AppleConfigurator/module.php
+++ IPS2AppleConfigurator/module.php
@@ -8,9 +8,8 @@
         {
             	// Diese Zeile nicht löschen.
             	parent::Create();
-		$this->ConnectParent("{66FD608F-6C67-6011-25E3-B9ED4C3E1590}");
-		$this->RegisterPropertyString("Location", '{"latitude":0,"longitude":0}');  
-		$this->RegisterPropertyFloat("Radius", 5.0);
+		$this->ConnectParent("{715318DA-1FA4-3CB4-2F0C-383322125646}");
+		
         }
  	
 	public function GetConfigurationForm() 
@@ -22,36 +21,29 @@
 		$arrayStatus[] = array("code" => 202, "icon" => "error", "caption" => "Kommunikationfehler!");
 				
 		$arrayElements = array(); 
-		$arrayElements[] = array("type" => "SelectLocation", "name" => "Location", "caption" => "Region");
-		$arrayElements[] = array("type" => "Label", "label" => "Radius (gemäß Tankerkönig.de Maximum 25 km)");
-		$arrayElements[] = array("type" => "NumberSpinner", "name" => "Radius", "caption" => "Radius (km)", "digits" => 1);
-		$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");
+		
 		$arraySort = array();
-		$arraySort = array("column" => "Brand", "direction" => "ascending");
+		$arraySort = array("column" => "Device", "direction" => "ascending");
 		
 		$arrayColumns = array();
-		$arrayColumns[] = array("caption" => "Marke", "name" => "Brand", "width" => "100px", "visible" => true);
-		$arrayColumns[] = array("caption" => "Name", "name" => "Name", "width" => "250px", "visible" => true);
-		$arrayColumns[] = array("caption" => "Strasse", "name" => "Street", "width" => "200px", "visible" => true);
-		$arrayColumns[] = array("caption" => "Ort", "name" => "Place", "width" => "auto", "visible" => true);
+		$arrayColumns[] = array("caption" => "Modell", "name" => "Device", "width" => "100px", "visible" => true);
+		$arrayColumns[] = array("caption" => "Name", "name" => "Name", "width" => "auto", "visible" => true);
 		
 		$StationArray = array();
 		If ($this->HasActiveParent() == true) {
-			$StationArray = unserialize($this->GetData());
+			$DevicenArray = unserialize($this->GetData());
 		}
 		$arrayValues = array();
 		for ($i = 0; $i < Count($StationArray); $i++) {
 			$arrayCreate = array();
-			$arrayCreate[] = array("moduleID" => "{47286CAD-187A-6D88-89F0-BDA50CBF712F}", 
-					       "configuration" => array("StationID" => $StationArray[$i]["StationsID"], "Timer_1" => 10));
-			$arrayValues[] = array("Brand" => $StationArray[$i]["Brand"], "Name" => $StationArray[$i]["Name"], "Street" => $StationArray[$i]["Street"],
-					       "Place" => $StationArray[$i]["Place"], "instanceID" => $StationArray[$i]["InstanceID"], 
+			$arrayCreate[] = array("moduleID" => "{4C40D461-8047-04BC-3566-52E76067225A}", 
+					       "configuration" => array("DeviceID" => $DeviceArray[$i]["DeviceID"]));
+			$arrayValues[] = array("Brand" => $DeviceArray[$i]["Device"], "Name" => $DeviceArray[$i]["Name"], 
+					       "instanceID" => $DeviceArray[$i]["InstanceID"], 
 					       "create" => $arrayCreate);
 		}
 		
-		$arrayElements[] = array("type" => "Configurator", "name" => "PetrolStations", "caption" => "Tankstellen", "rowCount" => 10, "delete" => false, "sort" => $arraySort, "columns" => $arrayColumns, "values" => $arrayValues);
-		$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");
-		$arrayElements[] = array("type" => "Button", "caption" => "Tankerkönig-API", "onClick" => "echo 'https://creativecommons.tankerkoenig.de/';");
+		$arrayElements[] = array("type" => "Configurator", "name" => "AppleDevices", "caption" => "Apple-Devices", "rowCount" => 10, "delete" => false, "sort" => $arraySort, "columns" => $arrayColumns, "values" => $arrayValues);
 		
  		return JSON_encode(array("status" => $arrayStatus, "elements" => $arrayElements)); 		 
  	}       
