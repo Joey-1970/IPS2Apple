1a39e1818e31cd577d80e580845ebf8a76a1635f
Joey-1970 <paeper@horburg.de>
2020-11-24 17:45:05 +0100

    Update module.php

--- IPS2AppleDevice/module.php
+++ IPS2AppleDevice/module.php
@@ -168,7 +168,7 @@
 			SetValueFloat($this->GetIDForIdent("verticalAccuracy"), $DeviceDataArray->location->verticalAccuracy);
 			SetValueFloat($this->GetIDForIdent("Longitude"), $DeviceDataArray->location->longitude);
 			$this->GoogleMaps($DeviceDataArray->location->latitude, $DeviceDataArray->location->longitude, $DeviceDataArray->name);
-			$this->GoogleMaps($DeviceDataArray->location->latitude, $DeviceDataArray->location->longitude, $DeviceDataArray->altitude);
+			$this->GPS_Distanz($DeviceDataArray->location->latitude, $DeviceDataArray->location->longitude, $DeviceDataArray->altitude);
         	} 
 		else {
 			SetValueString($this->GetIDForIdent("GoogleMaps"), "Karte konnte nicht erstellt werden (Keine aktuellen Daten).");
