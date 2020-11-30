d95f4fd2c8c8bbd3d56b059a3098c7592ff3cd87
Joey-1970 <paeper@horburg.de>
2020-11-30 20:34:53 +0100

    Update module.php

--- IPS2AppleDevice/module.php
+++ IPS2AppleDevice/module.php
@@ -149,7 +149,7 @@
   		switch($Ident) {
 	        	case "PlaySound":
 		    		$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{485663CC-3911-FAC7-9FCC-6E4D06438527}", 
-						"Function" => "PlaySound", "DeviceID" => $this->ReadPropertyString("DeviceID"), "Message" => "Test")));
+						"Function" => "PlaySound", "DeviceID" => $this->ReadPropertyString("DeviceID"), "Message" => "IP-Symcon")));
 	            	break;
 	        default:
 	            	throw new Exception("Invalid Ident");
